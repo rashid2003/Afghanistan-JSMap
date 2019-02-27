4ef6b61d0314bcdda334e8b6374e0df68944a9e8
rashid obaidi <31435017+rashid2003@users.noreply.github.com>
2019-02-27 19:23:07 +0800

    Create mapdata.js

---
+++ js/mapdata.js
@@ -0,0 +1,186 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+    //General settings
+		width: "300", //or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    pop_ups: "detect",
+    
+		//State defaults
+		state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+		//Location defaults
+		location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+		//Label defaults
+		label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+		//Zoom settings
+		zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+		//Popup settings
+		popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+		//Advanced settings
+		div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website"
+  },
+  state_specific: {
+    AFG1741: {
+      name: "Badghis",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    AFG1742: {
+      name: "Hirat"
+    },
+    AFG1743: {
+      name: "Bamyan"
+    },
+    AFG1744: {
+      name: "Balkh"
+    },
+    AFG1745: {
+      name: "Faryab"
+    },
+    AFG1746: {
+      name: "Jawzjan"
+    },
+    AFG1747: {
+      name: "Ghor"
+    },
+    AFG1748: {
+      name: "Sari Pul"
+    },
+    AFG1749: {
+      name: "Farah"
+    },
+    AFG1750: {
+      name: "Hilmand"
+    },
+    AFG1751: {
+      name: "Nimroz"
+    },
+    AFG1752: {
+      name: "Uruzgan"
+    },
+    AFG1753: {
+      name: "Uruzgan"
+    },
+    AFG1754: {
+      name: "Kandahar"
+    },
+    AFG1755: {
+      name: "Zabul"
+    },
+    AFG1757: {
+      name: "Ghazni"
+    },
+    AFG1758: {
+      name: "Khost"
+    },
+    AFG1759: {
+      name: "Paktika"
+    },
+    AFG1760: {
+      name: "Badakhshan"
+    },
+    AFG1761: {
+      name: "Nuristan"
+    },
+    AFG1762: {
+      name: "Kunar"
+    },
+    AFG1763: {
+      name: "Kunduz"
+    },
+    AFG1764: {
+      name: "Nangarhar"
+    },
+    AFG1765: {
+      name: "Takhar"
+    },
+    AFG1766: {
+      name: "Baghlan"
+    },
+    AFG1767: {
+      name: "Kabul"
+    },
+    AFG1768: {
+      name: "Kapisa"
+    },
+    AFG1769: {
+      name: "Parwan"
+    },
+    AFG1770: {
+      name: "Laghman"
+    },
+    AFG1771: {
+      name: "Logar"
+    },
+    AFG1772: {
+      name: "Parwan"
+    },
+    AFG1773: {
+      name: "Samangan"
+    },
+    AFG1774: {
+      name: "Wardak"
+    },
+    AFG3413: {
+      name: "Paktya"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "34.516667",
+      lng: "69.183333",
+      name: "Kabul"
+    }
+  }
+};
