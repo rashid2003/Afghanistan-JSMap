a85e80aff17f14022cfe6e612ab334a4547a1a52
rashid obaidi <31435017+rashid2003@users.noreply.github.com>
2019-02-27 19:33:08 +0800

    Update mapdata.js

--- js/mapdata.js
+++ js/mapdata.js
@@ -11,7 +11,7 @@ var simplemaps_countrymap_mapdata={
 		state_description: "State description",
     state_color: "#88A4BC",
     state_hover_color: "#3B729F",
-    state_url: "",
+    state_url: "/herat",
     border_size: 1.5,
     all_states_inactive: "no",
     all_states_zoomable: "yes",
